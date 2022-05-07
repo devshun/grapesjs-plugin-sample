9cf00b9e6e9f3c540c7cbd770f85b0768fc8542e
shunppe <70781900+shunppe@users.noreply.github.com>
2022-05-07 21:50:24 +0900

    add blocks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import 'grapesjs';
 
+import { blocks } from './blocks';
+
 export default grapesjs.plugins.add('gjs-plugin-sample', (editor, opts = {}) => {
-  return null;
+  let config = opts;
+
+  let defaults = {
+    blocks: ['link-blocks', 'quote', 'text-basic'],
+  };
+
+  for (let name in defaults) {
+    if (!(name in config)) config[name] = defaults[name];
+  }
+  blocks(editor, config);
 });
